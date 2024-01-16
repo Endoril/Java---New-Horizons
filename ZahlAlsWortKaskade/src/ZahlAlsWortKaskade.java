2e29cd8b728206963173756f492f97874aaf3d9b
Endoril <140304617+Endoril@users.noreply.github.com>
2024-01-16 14:53:32 +0100

    Add files via upload

---
+++ ZahlAlsWortKaskade/src/ZahlAlsWortKaskade.java
@@ -0,0 +1,28 @@
+/*
+ * Beispiel für eine Kaskade
+ */
+
+public class ZahlAlsWortKaskade {
+
+	public static void main(String[] args) {
+		int zahl = 6;
+		
+		System.out.println("Programm schreibt die Zahlen");
+		System.out.println("von 1 bis 5 in Buchstaben!");
+		System.out.print  (zahl+": ");
+
+		if (zahl == 1)
+			System.out.println("eins");
+		else if (zahl == 2)
+			System.out.println("zwei");
+		else if (zahl == 3)
+			System.out.println("drei");
+		else if (zahl == 4)
+			System.out.println("vier");
+		else if (zahl == 5)
+			System.out.println("fuenf");
+		else 
+			System.out.println("liegt ausserhalb des Bereiches!");
+	}
+
+}
